b31fc093ee8fa3e51347b337fdf2056a7e876786
DioufFLR <geoffreyfleur@gmail.com>
2023-01-16 16:58:02 +0100

    disc new

--- artists/disc_new.php
+++ artists/disc_new.php
@@ -13,7 +13,29 @@
     <button><a href="discs.php">Revenir aux discs</a></button>
 
     <form action="script_disc_ajout.php" method="post">
-        <input type="text">
+
+        <label for="name">Title</label><br>
+        <input type="text" name="name" id="name" >
+        <br>
+        <label for="artist">Artist</label><br>
+        <input type="text" name="artist" id="artist" >
+        <br>
+        <label for="year">Year</label><br>
+        <input type="text" name="year" id="year" >
+        <br>
+        <label for="genre">Genre</label><br>
+        <input type="text" name="genre" id="genre" >
+        <br>
+        <label for="label">Label</label><br>
+        <input type="text" name="label" id="label" >
+        <br>
+        <label for="price">Price</label><br>
+        <input type="text" name="price" id="price" >
+        <br>
+        <label for="file">Picture</label><br>
+        <input type="file" name="file">
+
+        <input type="submit">
     </form>
 
 </body>
