64e068d324d23b44fbef7ff6b3c59b620b6fb1b2
DioufFLR <geoffreyfleur@gmail.com>
2023-01-24 15:59:47 +0100

    disc_delete.php and script_disc_delete.php

--- artists/disc_delete.php
+++ artists/disc_delete.php
@@ -1,1 +1,35 @@
 <?php
+// On se connecte à la BDD via notre fichier db.php :
+require "db.php";
+$db = connexionBase();
+
+// On récupère l'ID passé en paramètre :
+$id = $_GET["id"];
+
+// On crée une requête préparée avec condition de recherche :
+$requete = $db->prepare("SELECT * FROM disc JOIN artist ON artist.artist_id = disc.artist_id WHERE disc_id=?");
+// on ajoute l'ID du disque passé dans l'URL en paramètre et on exécute :
+$requete->execute(array($id));
+
+// on récupère le 1e (et seul) résultat :
+$myArtist = $requete->fetch(PDO::FETCH_OBJ);
+
+// on clôt la requête en BDD
+$requete->closeCursor();
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Disc - delete</title>
+</head>
+<body>
+    <div class="button_delete_delete">
+        <button></button>
+    </div>
+</body>
+</html>
--- artists/script_disc_delete.php
+++ artists/script_disc_delete.php
@@ -1,1 +1,24 @@
 <?php
+$id = (isset($_POST['id']) && ($_POST['id'] != "")) ? $_POST['id'] : Null;
+
+// S'il n'y a pas eu de redirection vers le formulaire (= si la vérification des données est ok) :
+require "db.php";
+$db = connexionBase();
+
+try {
+    $requete = $db->prepare(/** @lang text */ "DELETE FROM disc WHERE disc_id = :id;");
+
+    $requete->execute(array($id));
+}
+
+// Gestion des erreurs
+catch (Exception $e) {
+    echo "Erreur : " . $requete->errorInfo()[2] . "<br>";
+    die("Fin du script (script_disc_ajout.php)");
+}
+
+// Si OK: redirection vers la page artists.php
+header("Location: discs.php");
+
+// Fermeture du script
+exit;
